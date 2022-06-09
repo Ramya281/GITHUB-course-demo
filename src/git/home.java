fd8af28caad6716bfe89553bccfdec1f4cd951a3
Ramya281 <you@example.com>
2022-06-09 14:58:38 +0530

    first commit

---
+++ src/git/home.java
@@ -0,0 +1,7 @@
+package git;
+
+public class home {
+	public static void main(String args[]) {
+		System.out.println("WELCOME");
+	}
+}
---
+++ src/git/login.java
@@ -0,0 +1,8 @@
+package git;
+
+public class login {
+	public static void main(String args[]) {
+	System.out.println("WELCOME");
+}
+
+}
