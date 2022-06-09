87a2ba93bfe10b706881757a9cba64fc7e5b2fc6
Ramya281 <you@example.com>
2022-06-09 15:38:13 +0530

    added /modified/ deleted files

--- src/git/home.java
+++ src/git/home.java
@@ -3,5 +3,7 @@ package git;
 public class home {
 	public static void main(String args[]) {
 		System.out.println("WELCOME");
+		System.out.println("welcome to the home page");
+		System.out.println("thanks for being with us");
 	}
 }
